057a83066f6a4a2d249f2b61944c63a57f78f739
Jason Worthy <109769584+4everWorthy@users.noreply.github.com>
2024-06-14 21:01:36 -0400

    1st commit

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,6 @@
 const hands = ['rock', 'paper', 'scissors'];
 function getHand() {
-    return hands[parseInt(Math.random() * 10) % 3];
+    return hands[parseInt(Math.random() * 10) % 3]; // math.random will give you number between 0 - 1 :: multiply by 10 to get a number between 0 - 10 :: the remain 3 to get 0, 1 or 2
 }
 
 const player1 = {
@@ -11,7 +11,14 @@ const player2 ={
         name: "Jane",
         getHand: getHand
     };
-
+const player3 ={
+    name: "Mary",
+    getHand: getHand
+};
+const player4 ={
+    name: "Tom",
+    getHand: getHand
+};
 
 function playRound(player1, player2) {
     const hand1 = player1.getHand();
@@ -38,9 +45,35 @@ function playRound(player1, player2) {
      }
 }
 
-const winner = playRound(player1, player2);
-if (winner) {
-    console.log(`The winner is ${winner.name}!`);
-} else {
-    console.log("It's a tie!");
+function playerGame(player1, player2, playuntil) {
+    let player1Wins = 0;
+    let player2Wins = 0;
+
+    while (player1Wins < playuntil && player2Wins < playuntil) {
+        const winner = playRound(player1, player2);
+        
+        if (winner === player1) {
+            player1Wins++;
+        } else if (winner === player2) {
+            player2Wins++;
+        }
+
+        console.log(`Score - ${player1.name}: ${player1Wins} - ${player2.name} : ${player2Wins}`);
+    }
+
+    return player1Wins === playuntil ? player1 : player2
 }
+
+function playTournament(players, playuntil) {
+    const [player1, player2, player3, player4] = players;
+
+    const winner1 = playerGame(player1, player2, playuntil);
+    const winner2 = playerGame(player3, player4, playuntil);
+
+    const tournamentWinner = playerGame(winner1, winner2, playuntil);
+
+    console.log(`${tournamentWinner.name} wins the tournament champion!`);
+}
+
+console.log("Playing a tournament with 4 players...");
+playTournament([player1, player2, player3, player4], 3);
